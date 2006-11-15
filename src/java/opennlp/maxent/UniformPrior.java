263abf1f47aa9e699a2d99b579b45bcfc6c48163
tsmorton <tsmorton>
2006-11-15 21:40:01 +0000

    updated to reflect interface change.

--- src/java/opennlp/maxent/UniformPrior.java
+++ src/java/opennlp/maxent/UniformPrior.java
@@ -9,21 +9,15 @@ public class UniformPrior implements Prior {
 
   private int numOutcomes;
   private double r;
-  
-  /**
-   * Creates a uniform prior of 1/N for each outcome where N is the specified number
-   * of outcomes.
-   * @param numOutcomes
-   */
-  public UniformPrior(int numOutcomes) {
-    this.numOutcomes = numOutcomes;
-    r = Math.log(1.0/numOutcomes);
-  }
-  
+    
   public void logPrior(double[] dist, int[] context) {
     for (int oi=0;oi<numOutcomes;oi++) {
       dist[oi] = r;
     }
   }
 
+  public void setLabels(String[] outcomeLabels, String[] contextLabels) {
+    this.numOutcomes = outcomeLabels.length;
+    r = Math.log(1.0/numOutcomes);
+  }
 }
